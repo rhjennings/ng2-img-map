cc0e9011adb9cbe731c59c3ab14e91fb07e4ecb2
rhjennings <rhjennings@gmail.com>
2018-04-02 17:38:19 -0400

    initial imp-map module definition

---
+++ ng2-img-map.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { ImgMapComponent } from './ng2-img-map';
+
+@NgModule({
+	declarations: [ImgMapComponent],
+	imports: [],
+	exports: [ImgMapComponent]
+})
+export class ImgMapComponentModule {}
